0df5389a7e3ac5df90bd76d0b0fb742ecb5ba5df
Sharon <A01379035@itesm.mx>
2017-10-16 08:56:14 -0500

    clase

--- Main.java
+++ Main.java
@@ -23,7 +23,7 @@ public class Main {
 	}
     public static int binarySearch(int values[],int target) {
         Integer min = 0;
-        Integer max = values.length - 1;
+        Integer max = values.length;
                      Integer mid;
                      while(min <= max){
                          mid = (min + max)/2;
@@ -38,7 +38,7 @@ public class Main {
                     return -1;
                  }
     
-   public static void Insertionsort(int values[]){
+  /* public static void Insertionsort(int values[]){
         for(int i = 1; i == values.length - 1; i++){
             for(int j = 0; j == values.length - 1; j++){
                 if (j < i && values[j] > values[i]){
@@ -68,76 +68,80 @@ public class Main {
             smallest = values[i];
             values[i] = swapsmallest;
         }
-    }   
+    }   */
+    public static void Bubblesort(int values[]){
+        boolean not_sorted = true;
+        while(not_sorted == false){
+            for(int i = 0; i == values.length -1; i++){
+                if(values[i] < values[i - 1]){
+                    int temp = values[i];
+                    values[i] = values[i -1];
+                    values[i - 1] = temp;
+                    not_sorted = true;
+                }
+            }
+        }  
+    }
+     public static void makeHeap(int values[]){
+                for(int i = 0; i < values.length; i++){
+                int index = i;
+                while(index != 0){
+                    int parent = (index - 1)/2;
+                    
+                    if(values[index] <= values[parent]){
+                        break;
+                    }
+                    int temp = values[index];
+                    values[index] = values[parent];
+                    values[parent] = temp;
+                    
+                    index = parent;
+                }
+            }
+     }
+     public static int removeTopItem(int values[], int count){
+                int result = values[0];
+                
+                values[0] = values[ count - 1];
+                
+                int index = 0;
+                while(true){
+                    int child1 = 2* index + 1;
+                    int child2 = 2* index + 2;
+                    
+                    if(child1 >= count){
+                        child1 = index;
+                    }
+                    if(child2 >= count){
+                        child2 = index;
+                    }
+                    if(values[index] >= values[child1] && values[index] >= values[child2]){
+                        break;
+                    }
+                    
+                    int swapChild = child1;
+                    if(values[child2] > values[child1]){
+                        swapChild = child2;
+                    }
+                    int temp = values[index];
+                    values[index] = values[swapChild];
+                    values[swapChild] = temp;
+                    
+                    index = swapChild;
+                } 
+                return result;
+     }
 	public static void main(String[] args) {
-           
-		System.out.println("Testing array reversing");
-		int[] inputArray = {1, 2, 3, 4, 5};
-		
-		Stack<Integer> stack = new Stack<Integer>();
-                for (int i=0;i<inputArray.length;i++){
-                    stack.push(inputArray[i]);
-                }
-		
-		int[] outputArray = new int[inputArray.length];
-		for (int i=0;i<inputArray.length;i++){
-                    outputArray[i]=stack.pop();
-                }
-                
-		System.out.println("Input: " + Arrays.toString(inputArray));
-		System.out.println("Output: " + Arrays.toString(outputArray));
-		System.out.println();
-		
-		System.out.println("Testing train ordering");
-		Stack<Integer> train = new Stack<Integer>();
-		train.push(3);
-		train.push(2);
-		train.push(1);
-		train.push(3);
-		train.push(2);
-		
-		System.out.println("Train as it arrived to the station: " + Arrays.toString(train.toArray()));
-                
-                train.pop();
-                train.pop();
-                train.pop();
-                train.pop();
-                train.push(3);
-                train.push(2);
-                train.push(2);
-                train.push(1);
-		
-		System.out.println("Train ordered by priority: " + Arrays.toString(train.toArray()));
-		System.out.println();
-		
-		Stack<Integer> towerOne = new Stack<Integer>();
-		Stack<Integer> towerTwo = new Stack<Integer>();
-		Stack<Integer> towerThree = new Stack<Integer>();
-		
-		towerOne.push(3);
-		towerOne.push(2);
-		towerOne.push(1);
-		
-		System.out.println("At the beginning");
-		System.out.println("Hanoi tower #1: " + Arrays.toString(towerOne.toArray()));
-		System.out.println("Hanoi tower #2: " + Arrays.toString(towerTwo.toArray()));
-		System.out.println("Hanoi tower #3: " + Arrays.toString(towerThree.toArray()));
-		
-		towerOne.pop();
-		towerTwo.push(3);
-                towerOne.pop();
-		towerTwo.push(2);
-                towerOne.pop();
-		towerTwo.push(1);
-                
-		System.out.println("At the end");
-		System.out.println("Hanoi tower #1: " + Arrays.toString(towerOne.toArray()));
-		System.out.println("Hanoi tower #2: " + Arrays.toString(towerTwo.toArray()));
-		System.out.println("Hanoi tower #3: " + Arrays.toString(towerThree.toArray()));
-		System.out.println();
-                
-               
-		
+            int uArray[] = {1, 0, 24, 18, -2, 10};
+             System.out.println(Arrays.toString(uArray));
+             makeHeap(uArray);
+             System.out.println(Arrays.toString(uArray));
+             System.out.println(removeTopItem(uArray, uArray.length));
+             System.out.println(Arrays.toString(uArray));
+             System.out.println(removeTopItem(uArray, uArray.length));
+             System.out.println(Arrays.toString(uArray));
+             System.out.println(removeTopItem(uArray, uArray.length));
+             System.out.println(Arrays.toString(uArray));
 	}
 }
         
