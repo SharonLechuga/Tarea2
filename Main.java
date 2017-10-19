5133bda55a1e2e452ec031404b838f17e3edb47e
Sharon <A01379035@itesm.mx>
2017-10-19 01:10:20 -0500

    algoritmos

--- Main.java
+++ Main.java
@@ -12,9 +12,9 @@ public class Main {
             int  target;
             
      public static int linearSearch(int values[],int target) {
-                    for(int i =0; i == values.length; i++ ){
+                    for(int i =0; i < values.length; i++ ){
                         if(values[i] == target)
-                            return 1;
+                            return i;
                         if(values[i] > target)
                             return -1;  
                     }
@@ -23,14 +23,13 @@ public class Main {
 	}
     public static int binarySearch(int values[],int target) {
         Integer min = 0;
-        Integer max = values.length;
-                     Integer mid;
+        Integer max = values.length - 1;
                      while(min <= max){
-                         mid = (min + max)/2;
+                        Integer mid = (min + max)/2;
                          
-                         if(target < mid)
-                             max = mid +1;
-                         else if (target > mid)
+                         if(target < values[mid])
+                             max = mid - 1;
+                         else if (target > values[mid])
                              min = mid +1;
                          else
                              return mid;
@@ -38,45 +37,56 @@ public class Main {
                     return -1;
                  }
     
-  /* public static void Insertionsort(int values[]){
-        for(int i = 1; i == values.length - 1; i++){
-            for(int j = 0; j == values.length - 1; j++){
-                if (j < i && values[j] > values[i]){
-                    int k = values[i];
-                    shiftRight();
-                }
+   public static void Insertionsort(int values[]){
+        for (int i = 1; i < values.length; ++i)
+        {
+            int store = values[i];
+            int j = i-1;
+ 
+            while (j >= 0 && values[j] > store)
+            {
+                values[j+1] = values[j];
+                j = j-1;
             }
+            values[j+1] = store;
         }
-    }          
+    }        
     
-    public static void shiftRight(int values [], int start, int end){
-		for(int j = values.length - 1; j > start; j--)
-			top[j] = top[j - 1];
-		
-		top[j] = null;
-    } 
+   /* public static void shiftRight(int values [], int start, int end){
+		for(int j = values.length; j > start; j--){
+                    values[j+1] = values[j];
+                    values[j+1] = start;
+                    j = j - 1;
+                }
+		start = end;
+    } */
+    
     public static void Selecttionsort(int values[]){
         int smallest;
         int swapsmallest;
-        for(int i = 0; i == values.length - 1; i++){
-            for(int j = 0; j >= i; j++){
+        for(int i = 0; i < values.length - 1; i++){
+            for(int j = 0; j < values.length; j++){
                 if(values[j] < values[i]){
-                    smallest = values[j];
-                    swapsmallest = smallest;
+                    smallest = j;
+                    swapsmallest = values[smallest];
+                    values[smallest] = values[i];
+                    values[i] = swapsmallest;
                 }
             }
-            smallest = values[i];
-            values[i] = swapsmallest;
         }
-    }   */
+    }   
     public static void Bubblesort(int values[]){
         boolean not_sorted = true;
-        while(not_sorted == false){
-            for(int i = 0; i == values.length -1; i++){
+        
+        while(not_sorted){
+            not_sorted = false;
+            
+            for(int i = 0; i < values.length; i++){
                 if(values[i] < values[i - 1]){
                     int temp = values[i];
                     values[i] = values[i -1];
                     values[i - 1] = temp;
+                    
                     not_sorted = true;
                 }
             }
@@ -131,17 +141,60 @@ public class Main {
                 } 
                 return result;
      }
+     
+          public static void heapsort(int values[]){
+                for(int i = 0; i < values.length; i--){
+                    int temp = values[0];
+                    values[0] = values[i];
+                    values[i] = temp;
+                    
+                }
+            }
+          
 	public static void main(String[] args) {
+            System.out.println("makeHeap");
             int uArray[] = {1, 0, 24, 18, -2, 10};
              System.out.println(Arrays.toString(uArray));
              makeHeap(uArray);
              System.out.println(Arrays.toString(uArray));
+             System.out.println("removeTopItem");
              System.out.println(removeTopItem(uArray, uArray.length));
              System.out.println(Arrays.toString(uArray));
              System.out.println(removeTopItem(uArray, uArray.length));
              System.out.println(Arrays.toString(uArray));
-             System.out.println(removeTopItem(uArray, uArray.length));
-             System.out.println(Arrays.toString(uArray));
+             
+             System.out.println("heapsort");
+            int dArray[] = {1, 0, 24, 18, -2, 10};
+             System.out.println(Arrays.toString(dArray));
+             heapsort(dArray);
+             System.out.println(Arrays.toString(dArray));
+             
+             System.out.println("LinearSearch");
+             int bArray[] = {2, 5, 9, 12, 18, 20};
+             System.out.println(Arrays.toString(bArray));
+             System.out.println(linearSearch(bArray, 20));
+             
+             System.out.println("BinarySearch");
+             System.out.println(Arrays.toString(bArray));
+             System.out.println(binarySearch(bArray, 2));
+             
+             System.out.println("InsertionSort");
+             int nArray[] = {1, 0, 24, 18, -2, 10};
+             System.out.println(Arrays.toString(nArray));
+             Insertionsort(nArray);
+             System.out.println(Arrays.toString(nArray));
+             
+             System.out.println("SelectionSort");
+             int hArray[] = {1, 0, 24, 18, -2, 10};
+             System.out.println(Arrays.toString(hArray));
+             Insertionsort(hArray);
+             System.out.println(Arrays.toString(hArray));
+             
+             System.out.println("BubbleSort");
+             int kArray[] = {0, 1, 2, 9, 3, 6, 5, 7, 8, 4};
+             System.out.println(Arrays.toString(kArray));
+             Insertionsort(kArray);
+             System.out.println(Arrays.toString(kArray));
 	}
 }
         
